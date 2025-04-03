0efe8b2a3307a16c110d6d19cd09396f7219be8d
Kim Valentin <co.unicorno@mail.ru>
2025-04-03 19:09:14 +0400

    chore: reorder imports to prevent style overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
-import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import "./globals.css";
 
 export default function RootLayout({children}:{children:ReactNode}) {
   return children;
